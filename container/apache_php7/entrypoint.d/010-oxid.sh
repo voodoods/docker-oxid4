e7626c734f1c67845493691832bbb678d08c1796
Norman Steger <norman.steger@8select.de>
2019-02-01 17:40:37 +0100

    replace oxid 6 create-project with oxid 4 zip fetch

--- container/apache_php7/entrypoint.d/010-oxid.sh
+++ container/apache_php7/entrypoint.d/010-oxid.sh
@@ -4,7 +4,9 @@ set -e
 # bootstrap oxid files/project
 if [ ! -f /var/www/html/source/config.inc.php ]
 then
-	composer create-project --keep-vcs oxid-esales/oxideshop-project . ${OXID_VERSION}
+	wget https://oxidforge.org/wp-content/uploads/software/OXID_ESHOP_CE_4.10.8.zip
+	unzip -o OXID_ESHOP_CE_4.10.8.zip -d /var/www/html/source/
+	rm OXID_ESHOP_CE_4.10.8.zip 
 	mv /var/www/config.inc.php /var/www/html/source/config.inc.php
 	chown -R www-data:www-data /var/www/html/source/
 	echo "#####################################"
